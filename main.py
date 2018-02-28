8b729aeb2e17d410b021ea1ec38ab42058facfa4
Nathan Withers <nathanwithz@gmail.com>
2018-02-27 19:53:28 -0500

    Added rudimentary meaning definitions

--- main.py
+++ main.py
@@ -29,13 +29,59 @@ class Bot(discord.Client):
                 image_file = await compile_tex(content)
 
                 await self.send_file(message.channel, image_file)
+            # TODO add a translate command using vb.translate('word','en','fra')
             elif content.startswith('>define'):
-                # TODO get dictionary data of word with vocabulary
                 # TODO populate embed with correct data
-                em = discord.Embed(title='My Embed Title', description='My Embed Content.', colour=0xDEADBF)
-                em.set_author(name='Someone', icon_url=self.user.default_avatar_url)
+                # TODO create sub commands
+                # TODO hopefully fix duplicates
+                # and ignore everything after the first white space
+                args = content[7:].strip().split()
+                # if nothing was entered after the command, do nothing
+                if len(args) == 0:
+                    await self.delete_message(message)
+                    return
+                # get the first word typed after the command
+                word = args[0]
+                meanings = vb.meaning(word, format='list')
+                # if the word cannot be found to have any meaning then
+                # make an embed showing that. 
+                if not meanings:
+                    em = discord.Embed(title='Definition of ' + word, description='No definition could be found', colour=0xFF0000)
+                    await self.send_message(message.channel, embed=em)
+                    await self.delete_message(message)
+                    return
+                synonyms = vb.synonym(word, format='list')
+                antonyms = vb.antonym(word, format='list')
+                # part of speech. Not piece of shit :P
+                pos = vb.part_of_speech(word, format='list')
+                examples = vb.usage_example(word, format='list')
+                pronunciations = vb.pronunciation(word, format='list')
+                em = discord.Embed(title='Definition of ' + word, colour=0x00FF00)
+                for x in range(1, len(meanings) - 1):
+                    if x % 25 == 0:
+                        await self.send_message(message.channel, embed=em)
+                        em = discord.Embed(title='Definition of ' + word, colour=0x00FF00)
+                    em.add_field(name='Meaning ' + str(x), value=remove_html(meanings[x]), inline=False)
                 await self.send_message(message.channel, embed=em)
 
+
+# remove html tags while preserving quotes.
+def remove_html(s):
+    tag = False
+    quote = False
+    out = ""
+
+    for c in s:
+        if c == '<' and not quote:
+            tag = True
+        elif c == '>' and not quote:
+            tag = False
+        elif (c == '"' or c == "'") and tag:
+            quote = not quote
+        elif not tag:
+            out = out + c
+
+    return out
 
 async def compile_tex(snippet):
     async with aiofiles.open('template.tex') as f:
